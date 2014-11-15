657ccedb28cc4a6a13a3a9b595a4b4e0ed400841
niakod <niakodmaria@gmail.com>
2014-11-15 00:29:56 -0200

    clase sistema

--- GestionApicola/src/Dominio/Sistema.java
+++ GestionApicola/src/Dominio/Sistema.java
@@ -1,7 +1,42 @@
 package Dominio;
 /**
- * Fachada del sistema.
+ * Fachada del sistema. Utiliza Singleton.
  */
 public class Sistema {
-
+	/**
+	 * Instancia única del sistema.
+	 */
+	private static Sistema instancia = null;
+	/**
+	 * Cantidad máxima de puntos que soportará el sistema.
+	 */
+	private int cantPuntos;
+	/**
+	 * Array de puntos del sistema.
+	 */
+	private Punto[] arrPuntos;
+	/**
+	 * Lista de tramos del sistema.
+	 */
+	private ListaTramos lstTramos;
+	/**
+	 * Arbol de apicultores del sistema.
+	 */
+	private ArbolApicultores treApicultores;
+	/**
+	 * Constructor de Sistema, no recibe parámetros.
+	 */
+	private Sistema(){
+		
+	}
+	/**
+	 * Obtiene la instancia del sistema o crea una nueva si la actual es nula.
+	 * @return Instancia del sistema.
+	 */
+	public static Sistema getInstancia(){
+		if (instancia == null){
+			instancia = new Sistema();
+		}
+		return instancia;
+	}
 }
